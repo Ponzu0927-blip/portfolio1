22ae0a2584e8fde7fceba958f65285466f7c52c7
みなみ 矢野 <ponzu0927@icloud.com>
2025-10-23 13:26:26 +0900

    余分なusingステートメントの削除

--- ポートフォリオ作成/Info.cs
+++ ポートフォリオ作成/Info.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Collections.Generic;
 namespace ポートフォリオ作成
 {
     public partial class Info : Form
